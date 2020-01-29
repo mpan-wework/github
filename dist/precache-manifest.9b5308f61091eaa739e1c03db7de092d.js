ab1aba52cb81325a4efb9cd95958fbe66a4867a3
mpan-wework <mike.pan@wework.com>
2020-01-29 22:14:53 +0800

    dist

---
+++ dist/precache-manifest.9b5308f61091eaa739e1c03db7de092d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b87246267e1875f23192d4eeebc335fd",
+    "url": "/github/dist/index.html"
+  },
+  {
+    "revision": "8abb6fae193084f591bd",
+    "url": "/github/dist/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "26ff484c4615d7ae4a52",
+    "url": "/github/dist/static/js/2.0a43678d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/github/dist/static/js/2.0a43678d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8abb6fae193084f591bd",
+    "url": "/github/dist/static/js/main.9e9d79a9.chunk.js"
+  },
+  {
+    "revision": "b114877b9096acb3e675",
+    "url": "/github/dist/static/js/runtime-main.d3f82878.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github/dist/static/media/logo.5d5d9eef.svg"
+  }
+]);
