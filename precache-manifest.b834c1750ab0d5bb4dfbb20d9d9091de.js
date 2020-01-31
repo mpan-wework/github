8dd2e16f65c7d8cdb7123b7fa1a8971606d53ea4
bot <>
2020-01-31 10:37:36 +0000

    test

---
+++ precache-manifest.b834c1750ab0d5bb4dfbb20d9d9091de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24ca5379d38a8ba1470dd0bcefa3fb6e",
+    "url": "/github/index.html"
+  },
+  {
+    "revision": "6bbc7c8db86f33cd55f8",
+    "url": "/github/static/css/main.30d5de11.chunk.css"
+  },
+  {
+    "revision": "c53b6a4a63d0993de2b9",
+    "url": "/github/static/js/2.78cb3caf.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/github/static/js/2.78cb3caf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6bbc7c8db86f33cd55f8",
+    "url": "/github/static/js/main.c6eb9285.chunk.js"
+  },
+  {
+    "revision": "eca9c286cc83d9832e24",
+    "url": "/github/static/js/runtime-main.e255d01a.js"
+  }
+]);
